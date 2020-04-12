ff8f242765440c6d47fc82f670448c77cb6ad7da
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-04-12 11:12:06 +0900

    Move var_dump

--- sample.php
+++ sample.php
@@ -3,8 +3,8 @@ class Animal
 {
   public function bark()
   {
-    echo 'Yeah, it’s barking.' . PHP_EOL;
     var_dump('----------1-----------');
+    echo 'Yeah, it’s barking.' . PHP_EOL;
   }
 }
 
@@ -15,9 +15,9 @@ class Dog extends Animal
 
   public function __construct($name, $age=1)
   {
+    var_dump('----------2-----------');
     $this->name = $name;
     $this->age = $age;
-    var_dump('----------2-----------');
   }
 }
 
@@ -27,20 +27,22 @@ class MechaDog extends Dog
 
   public function __construct($name, $age=1)
   {
+    var_dump('----------3-----------');
     parent::__construct($name);
     $this->data = array(
       'apache' => 'apache',
       'bsd' => 'mit',
       'chef' => 'apache'
     );
-    var_dump('----------3-----------');
   }
 
   public function proc($arg)
   {
+    var_dump('----------4-----------');
     $path = explode("/", explode(" ", $arg)[0]);
     array_shift($path);
     if( is_null($path) ) {
+      var_dump('----------5-----------');
       $keys = array();
       while (list($key, $val) = each($this->data)) {
         array_push($keys, $key);
@@ -48,13 +50,14 @@ class MechaDog extends Dog
       var_dump($keys);
     }
     else if(count($path) == 2){
+      var_dump('----------6-----------');
       $this->data[$path[0]] = $path[1];
       echo $path[1] . PHP_EOL;
     }
     else {
+      var_dump('----------7-----------');
       echo $path[0] . "=>" . $this->data[$path[0]] . PHP_EOL;
     }
-    var_dump('----------4-----------');
   }
 }
 
